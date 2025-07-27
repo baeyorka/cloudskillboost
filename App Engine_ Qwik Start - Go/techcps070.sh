85122684f80e64aa037ebe57e1d8e504a8d2b7e4
Arif Hidayat <58173859+baeyorka@users.noreply.github.com>
2025-07-27 13:53:54 +0700

    Add files via upload

---
+++ App Engine_ Qwik Start - Go/techcps070.sh
@@ -0,0 +1,23 @@
+
+
+gcloud auth list
+
+gcloud services enable appengine.googleapis.com
+
+export ZONE=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
+
+export REGION=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-region])")
+
+export PROJECT_ID=$(gcloud config get-value project)
+
+gcloud config set compute/region $REGION
+
+git clone https://github.com/GoogleCloudPlatform/golang-samples.git
+
+cd golang-samples/appengine/go11x/helloworld
+
+sudo apt-get install google-cloud-sdk-app-engine-go
+
+gcloud app create --project=$PROJECT_ID --region=$REGION
+
+gcloud app deploy --project=$PROJECT_ID --quiet
